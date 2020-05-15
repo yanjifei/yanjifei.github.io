a357beb9f79ff89ff1df4d77cdb8960f294d520d
Jay Yan <yanjifei@hotmail.com>
2020-05-15 20:19:48 +0100

    website

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Test",
+        "excerpt":"  Test   you can use an inline formula \\(\\forall x \\in R\\) like this one   \\[M = \\left( \\begin{array}{ccc} x_{11} &amp; x_{12} &amp; \\ldots \\\\ x_{21} &amp; x_{22} &amp; \\ldots \\\\ \\vdots &amp; \\vdots &amp; \\ldots \\\\ \\end{array} \\right)\\]  ","categories": ["FPGA"],
+        "tags": ["update"],
+        "url": "http://localhost:4000/fpga/test/",
+        "teaser": "http://localhost:4000/assets/images/500x300.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": ["update"],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser": "http://localhost:4000/assets/images/500x300.jpg"
+      }]
--- index.markdown
+++
@@ -1,7 +0,0 @@
----
-# Feel free to add content and custom Front Matter to this file.
-# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-callouts: home_callouts
-show_sidebar: true
-layout: home
----
