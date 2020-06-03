ca74bcf2483d058e16e57876f7c11e181ded03e1
Jay Yan <jifei.yan@ncr.com>
2020-06-03 08:45:29 +0100

    commit build

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "PID Controller in FPGA For DC Motors (Part 1)",
         "excerpt":"I recently work on a project which requires a customized controller in FPGA to control a DC motor. Since PID controller is a good start point for this type of control function, I decided to refresh my control theory knowledge before I start implementing any design. I found a webpage...","categories": ["FPGA"],
         "tags": ["DC Motor","PID"],
-        "url": "http://localhost:4000/fpga/pid-control-dc-motor-p1/",
-        "teaser": "http://localhost:4000/assets/images/500x300.jpg"
+        "url": "https://yanjifei.github.io/fpga/pid-control-dc-motor-p1/",
+        "teaser": "https://yanjifei.github.io/assets/images/500x300.jpg"
       }]
